3b4f0648046f5850cfbe0e5a848483331c62d9af
naipaka <naipakapaka@gmail.com>
2020-03-20 23:42:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Udacity Online Flutter Training
---
+++ lesson1/hello_rectangle/android/app/src/main/kotlin/com/naipaka/hello_rectangle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naipaka.hello_rectangle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
